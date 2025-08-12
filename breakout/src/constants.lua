16a0d2d3935d5f51444bd521acfd8586c3a68297
Mridul Ahluwalia <ahluwalia.mridul@gmail.com>
2025-08-12 12:34:10 +0530

    checkpoint

---
+++ breakout/src/constants.lua
@@ -0,0 +1,10 @@
+-- window size
+WINDOW_WIDTH = love.graphics.getWidth()
+WINDOW_HEIGHT = love.graphics.getHeight()
+
+-- virtual window size
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- paddle movement speed
+PADDLE_SPEED = 200
